4d33c8ebdd2686af83ae063920d506595f080db6
dandyh <dandy.masyaril@gmail.com>
2017-02-01 18:53:55 +0000

    First Commit

--- WPFLogin/Face.cs
+++ WPFLogin/Face.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 namespace WPFLogin
 {
     /// <summary>
-    /// Face view model
+    /// Face view model Dandy Handoko
     /// </summary>
     public class Face : INotifyPropertyChanged
     {
